19e2b9760e92faeaca776588eeb39fcb55524184
Alphasec <95212748+alphasecio@users.noreply.github.com>
2023-02-27 00:38:40 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+
 const app = express();
 const PORT = 3000;
 
-app.use(express.json());
-app.use(express.urlencoded());
+app.use(bodyParser.json());
 
 app.get("/", function(req, res) {
   console.log(req.body);
