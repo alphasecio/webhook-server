65c9e99e7102392a7e356ac6bd8cebbff7a3b82a
Alphasec <95212748+alphasecio@users.noreply.github.com>
2023-02-27 00:04:25 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const PORT = 3000;
+
+const app = express();
+
+app.get("/", function(req, res) {
+  res.send("Welcome to the Webhook Server!");
+});
+
+app.post("/webhook-1", function(req, res) {
+  console.log(req.body);
+  res.send("Webhook 1 successfully received.");
+});
+
+app.post("/webhook-2", function(req, res) {
+  console.log(req.body);
+  res.send("Webhook 2 successfully received.");
+});
+
+app.post("/webhook-3", function(req, res) {
+  console.log(req.body);
+  res.send("Webhook 3 successfully received.");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at https://localhost:${PORT}/`);
+});
