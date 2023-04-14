327c79175c9b215a5e0c8856a425ce7158b8ba80
Alphasec <95212748+alphasecio@users.noreply.github.com>
2023-04-14 19:46:15 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,30 +2,39 @@ const express = require("express");
 
 // Create an Express app and listen for incoming requests on port 3000
 const app = express();
-const PORT = 3000;
+const router = express.Router();
+const port = process.env.PORT || 3000;
 
 // Use middleware to parse incoming requests with JSON and URL-encoded payloads
 app.use(express.json());
 app.use(express.urlencoded());
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Internal Server Error");
+});
+
 // Handle GET requests to the root URL
-app.get("/", function(req, res) {
-  console.log(req.body);
+router.get("/", (req, res) => {
   res.send("Welcome to the Webhook Server!");
 });
 
 // Handle POST requests to specific URLs i.e. webhook endpoints
-app.post("/webhook-1", function(req, res) {
+router.post("/webhook-1", (req, res) => {
   console.log(req.body);
   res.send("Webhook 1 successfully received.");
 });
 
-app.post("/webhook-2", function(req, res) {
+router.post("/webhook-2", (req, res) => {
   console.log(req.body);
   res.send("Webhook 2 successfully received.");
 });
 
+// Mount the router middleware
+app.use(router);
+
 // Start the server and listen for incoming connections
-app.listen(PORT, () => {
-  console.log(`Server running at https://localhost:${PORT}/`);
+app.listen(port, () => {
+  console.log(`Server running at https://localhost:${port}/`);
 });
